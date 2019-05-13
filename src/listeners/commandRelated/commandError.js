ae9f4118d75d813321320120e9427101fea5fc5d
xem <xem.discord.gg@gmail.com>
2019-05-13 15:47:30 -0300

    Update to post to log, and some other channel.

--- src/listeners/commandRelated/commandError.js
+++ src/listeners/commandRelated/commandError.js
@@ -1,6 +1,7 @@
 const { Listener } = require('discord-akairo');
 const chalk = require('chalk');
 const moment = require('moment');
+const fs = require('fs');
 const { errorMessage } = require('../../utils/embeds');
 
 class CommandErrorListener extends Listener {
@@ -17,9 +18,20 @@ class CommandErrorListener extends Listener {
 
         // Create the log itself with Chalk and Moment
         let log = `${timestamp} | ${chalk.bgRed.bold(`ERROR - ${command}`)} | ${error.message}`;
+        let otherLogs = message.guild.channels.find(channel => channel.name === 'other-logs')
+            .catch((err) => console.error(err));
 
         // Send the error then log it
-        errorMessage(`**Ha ocurrido un error:**\n\`\`\`js\n${error.message}\`\`\``, message)
+        errorMessage(`**Ha ocurrido un error:**\n\`\`\`js\n${error.message}\`\`\``, message);
+        otherLogs.send(`<@${this.client.ownerID}>`, errorMessage(`**Ha ocurrido un error en el canal <#${message.channel.id}>**\n\`\`\`js\n${error.message}\`\`\``, message));
+        
+        // AppendFile to Error Log
+        fs.appendFile('src/listeners/errors/errors.log', `\r\n============`
+        + `\r\nERROR`
+        + `\r\n------------`
+        + `\r\n${error.stack}`
+        + `\r\n============`);
+
         console.log(log);
     }
 }

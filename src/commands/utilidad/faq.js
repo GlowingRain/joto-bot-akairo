7550fa98a75a84bf4b3aee6902b8cc4cfbba2ea8
xem <xem.discord.gg@gmail.com>
2019-05-21 18:42:38 -0300

    FAQ Command

---
+++ src/commands/utilidad/faq.js
@@ -0,0 +1,50 @@
+const { Command } = require('discord-akairo');
+const Values = require('../../storage/questions.js');
+const { MessageEmbed } = require('discord.js');
+
+class FAQCommand extends Command {
+    constructor() {
+        super('faq', {
+            aliases: ['faq', 'frequently', 'frecuente', 'preguntas'],
+            args: [
+                {
+                    id: 'questions',
+                    prompt: {
+                        start: message => {
+                            let embed = new MessageEmbed()
+                                .setTitle('⚜ FAQ & Acceso Rápido')
+                                .setColor('RANDOM')
+                                .setDescription('- **XP**'
+                                    + '\n- **Respeto**'
+                                    + '\n- **Bardo**'
+                                    + '\n- **Spam**'
+                                    + '\n- **Flood**'
+                                    + '\n- **Exploit**'
+                                    + '\n- **Pacman**'
+                                    + '\n- **N-WORD**')
+                                .setFooter('Selecciona una opción escribiendo lo que quieras leer.');
+
+                            return { embed };
+                        },
+                        retry: 'No se ha encontrado algo parecido a eso.'
+                    }
+                }
+            ]
+        })
+    }
+
+    exec(message, args) {
+        if (args.questions) {
+            let result = Values.questions.responses[args.questions];
+            if (result === undefined) return null;
+            let nEmbed = this.client.util.embed()
+                .setTitle('⚜ ' + args.questions.toUpperCase())
+                .setColor('RANDOM')
+                .setDescription(result);
+
+            return message.channel.send(nEmbed);
+        }
+    }
+}
+
+module.exports = FAQCommand;
---
+++ src/storage/questions.js
@@ -0,0 +1,14 @@
+module.exports = {
+    questions: {
+        responses: {
+            "xp": "Recibes 15XP por cada mensaje que envías dentro de un intervalo de 1 minuto. Pasado ese minuto vuelves a obtener los 15XP si mandas un mensaje.",
+            "respeto": "Respetar a los usuarios y el staff. Cabe destacar que muchas de las normas por más que no estén escritas, poseen de su lado el sentido común, usa la cabeza y no seas uno más del montón.",
+            "bardo": "No hacer **\"bardo\"**. Las peleas sin razón aparenta no se van a perdonar, cualquier tipo de bardo tampoco.",
+            "spam": "El spam está estrictamente prohibido; de esto no se exceptúan las invitaciones innecesarias a un servidor de Discord.",
+            "flood": "Ningún tipo de flood está permitido, desde cadenas hasta frases sin sentido alguno.",
+            "exploit": "Abusar/Exploitear de los servicios del servidor está prohibido. Ej.: El abusar del sistema de XP, el uso de multicuentas al ser baneado en la cuenta principal, también destacando el abusar de un rol, entre otros.",
+            "pacman": "El pacman está permitido siempre y cuando no se use en exceso.",
+            "n-word": "La palabra con N en su versión inglesa está totalmente prohibida debido a que formamos y tenemos un **servidor socio** (partner) con Discord, por lo que debemos apegarnos al **Código de Conducta ded Partnership** y las **Directivas de la Comunidad**, tanto como los **Términos y Condiciones** del mismo servicio."
+        }
+    },
+}
